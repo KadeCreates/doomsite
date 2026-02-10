efa4315bd664a954bfe6f77bcb58955078c3bd63
doom <126303901+KadeCreates@users.noreply.github.com>
2026-02-09 20:37:44 -0500

    Add font imports and configurations for display and mono

--- src/app/fonts/fonts.ts
+++ src/app/fonts/fonts.ts
@@ -1,1 +1,15 @@
+import { JetBrains_Mono, Cormorant_Garamond } from 'next/font/google'
 
+export const displayFont = Cormorant_Garamond({
+  weight: ['300', '400', '600'],
+  subsets: ['latin'],
+  variable: '--font-display',
+  display: 'swap',
+})
+
+export const monoFont = JetBrains_Mono({
+  weight: ['300', '400'],
+  subsets: ['latin'],
+  variable: '--font-mono',
+  display: 'swap',
+})
